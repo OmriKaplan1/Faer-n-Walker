ccf95b2024ab3b41642869bbfe8f2511f2a5355b
OmriKaplan1 <116948664+OmriKaplan1@users.noreply.github.com>
2025-08-16 17:36:23 +0300

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,16 @@
+// Reveal on scroll
+function revealOnScroll() {
+  const reveals = document.querySelectorAll('.reveal');
+  const windowHeight = window.innerHeight;
+  const revealPoint = 100; // trigger point before bottom
+
+  reveals.forEach(el => {
+    const elementTop = el.getBoundingClientRect().top;
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
