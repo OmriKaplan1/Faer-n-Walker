966b7832a70190efdaca5384fb3d6aacddb12404
OmriKaplan1 <116948664+OmriKaplan1@users.noreply.github.com>
2025-08-25 11:42:27 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -15,20 +15,4 @@ function revealOnScroll() {
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-});
+
